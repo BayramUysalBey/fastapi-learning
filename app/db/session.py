fbdf50d9cdbcb1b17ed11bcf7afeeb55eee35781
Bayram Uysal <122267966+BayramUysalBey@users.noreply.github.com>
2026-04-30 13:52:57 +0300

    feat: implement database integration, user authentication with OAuth2, and password hashing support (#7)

--- app/db/session.py
+++ app/db/session.py
@@ -1,6 +1,28 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+import asyncio
+from typing import AsyncGenerator
+from sqlalchemy import MetaData
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from app.core.settings import settings
+import logging
+
+logger = logging.getLogger(__name__)
 
 
+
+meta = MetaData()
 engine = create_async_engine(settings.DATABASE_URL, echo=True)
-SessionLocal = async_sessionmaker(bind=engine, autocommit=False, autoflush=False, expire_on_commit=False)
+AsyncSessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
+
+			
+async def get_db() -> AsyncGenerator:
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        except Exception as e:
+            await session.rollback()
+            logger.error(e, exc_info=e)
+            raise e
+        else:
+            await session.commit()
+        finally:
+            await session.close()
